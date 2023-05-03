fa03de8437188542fe1ee050a3d7838f1c32aa36
Eduard Jiron <eduard.jiron13901@est.uca.edu.ni>
2023-05-03 14:40:12 -0600

    entidades añadidias

---
+++ app/src/main/java/com/twelvehours/miproyectofinal/entidades/Repositories.kt
@@ -0,0 +1,10 @@
+package com.twelvehours.miproyectofinal.entidades
+
+data class Repositories(
+    val id:Int,
+    val name:String,
+    val description:String,
+    val language:String,
+    val commits:Int,
+
+)
---
+++ app/src/main/java/com/twelvehours/miproyectofinal/entidades/User.kt
@@ -0,0 +1,10 @@
+package com.twelvehours.miproyectofinal.entidades
+
+data class User(
+    val id:Int,
+    val name:String,
+    val followers:String,
+    val following:String,
+    val numberRepositories:Int,
+
+)
